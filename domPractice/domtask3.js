b3015e4ae8cfa9a4db8d2c5622caa74899c1fa92
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-01-05 02:45:10 +0530

    task 4 dom manipulations

--- domPractice/domtask3.js
+++ domPractice/domtask3.js
@@ -25,6 +25,20 @@ var headerTitle = document.getElementById('header-title');
 //headerTitle.innerHTML='<h3>Hello</h3>';
 headerTitle.style.borderBottom='solid 3px #000';
 let addItem = document.getElementById('main');
-addItem.style.color='green';
-addItem.style.fontWeight='bold';
+//addItem.style.color='green';
+//addItem.style.fontWeight='bold';
 // GETELEMENTSBYCLASSNAME //
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor = '#f4f4f4';
+    items[i].style.fontWeight='bold';
+}
+items[2].style.backgroundColor = 'green';
