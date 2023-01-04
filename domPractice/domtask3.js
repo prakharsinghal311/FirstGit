2f2cfc724d8a8047824cda2c3cae3fd95c8f9290
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-01-05 03:13:12 +0530

    dom manipulations task 5

--- domPractice/domtask3.js
+++ domPractice/domtask3.js
@@ -17,28 +17,49 @@
 
 // GETELEMENTBYID //
 //console.log(document.getElementById('header-title'));
-var headerTitle = document.getElementById('header-title');
+//var headerTitle = document.getElementById('header-title');
 //console.log(headerTitle)
 // headerTitle.textContent='Hello';
 // headerTitle.innerText='GoodBye';
 //console.log(headerTitle.innerText);
 //headerTitle.innerHTML='<h3>Hello</h3>';
-headerTitle.style.borderBottom='solid 3px #000';
-let addItem = document.getElementById('main');
+//headerTitle.style.borderBottom='solid 3px #000';
+//let addItem = document.getElementById('main');
 //addItem.style.color='green';
 //addItem.style.fontWeight='bold';
 // GETELEMENTSBYCLASSNAME //
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+var li = document.getElementsByClassName('list-group-item');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
 
-for(var i=0;i<items.length;i++)
+for(var i=0;i<li.length;i++)
 {
-    items[i].style.backgroundColor = '#f4f4f4';
-    items[i].style.fontWeight='bold';
+    li[i].style.backgroundColor = '#f4f4f4';
+    li[i].style.fontWeight='bold';
 }
-items[2].style.backgroundColor = 'green';
+li[2].style.backgroundColor = 'green';
+
+// GETELEMENTSBYTAGNAME //
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor = '#f4f4f4';
+    li[i].style.fontWeight='bold';
+}
+li[2].style.backgroundColor = 'green';
+
+// QUERYSELECTOR //
+
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
